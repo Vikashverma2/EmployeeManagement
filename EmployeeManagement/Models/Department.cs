4e1a6ccce3d1d9aae91015b23bf1c87b1d19e9b2
vikashverma3 <vikashvermagit@gmail.com>
2025-10-17 19:24:44 +0530

    Add models, repositories, and services structure

---
+++ EmployeeManagement/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Employee>? Employees { get; set; }
+
+    }
+}
