fbb4d9e4e7e3a2d41e13f82401853ecd88e57a2e
vikashverma2 <vikashvermagit@gmail.com>
2025-11-08 21:18:07 +0530

    Added some XunitTest Cases

---
+++ EmployeeManagement.Tests/Controllers/DepartmentControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeManagement.Controllers;
+using EmployeeManagement.Models;
+using EmployeeManagement.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Tests.Controllers
+{
+    public class DepartmentControllerTests
+    {
+        private readonly Mock<IDepartmentService> mockService;
+        private readonly Mock<ILogger<DepartmentController>> mockLogger;
+        private readonly DepartmentController controller;
+
+        public DepartmentControllerTests()
+        {
+            mockService = new Mock<IDepartmentService>();
+            mockLogger = new Mock<ILogger<DepartmentController>>();
+
+            controller = new DepartmentController
+            (
+                mockService.Object,
+                mockLogger.Object
+            );
+        }
+
+
+
+    }
+}
